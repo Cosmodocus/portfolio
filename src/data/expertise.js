865d00378427c1b862a626af5c3f43877407ae89
Reza <homayunir@gmail.com>
2024-09-16 16:38:27 -0400

    updated expertise card styling as well as abstracted expertiseData

---
+++ src/data/expertise.js
@@ -0,0 +1,21 @@
+import expertise1 from "../assets/expertise1.svg";
+import expertise2 from "../assets/expertise2.svg";
+import expertise3 from "../assets/expertise3.svg";
+
+export const expertiseData = [
+    {
+      title: "Frontend Developer",
+      description: "Specialized in creating responsive and interactive user interfaces using modern frontend technologies.",
+      icon: expertise1
+    },
+    {
+      title: "Fullstack Developer",
+      description: "Experienced in building end-to-end solutions, handling both frontend and backend development.",
+      icon: expertise2,
+    },
+    {
+      title: "Tech Lead",
+      description: "Proven track record of leading development teams, managing projects, and guiding technical decisions.",
+      icon: expertise3,
+    },
+  ];
