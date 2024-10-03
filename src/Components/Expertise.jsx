0ebdc1003c1913a637b403e41897bdef07e2b430
Reza <homayunir@gmail.com>
2024-10-03 19:04:51 -0400

    Updates:     - Reworked copy for Expertise Section     - Reworked Styling for Expertise Section     - Upgraded the glowing animation border

--- src/Components/Expertise.jsx
+++ src/Components/Expertise.jsx
@@ -9,7 +9,7 @@ const Expertise = () => {
   useEffect(() => {
     // Start the glowing effect for the border
     borderControls.start({
-      borderColor: ["#4ae6e6", "#1fc6c6", "#4ae6e6"], // Change to your desired colors
+      borderColor: ["#9ee3e3", "#1fc6c6", "#9ee3e3"], // Use lighter colors for better visibility in dark mode
       transition: {
         duration: 1.5,
         repeat: Infinity,
@@ -19,7 +19,7 @@ const Expertise = () => {
 
     // Start the glowing effect for the title
     titleControls.start({
-      opacity: [2, 0.7, 1], // Fading effect for the title
+      opacity: [1, 0.7, 1], // Use a more subtle opacity range for the title
       transition: {
         duration: 1.5,
         repeat: Infinity,
@@ -30,30 +30,56 @@ const Expertise = () => {
 
   return (
     <div className="relative container mx-auto px-6 py-32 flex flex-col items-center justify-center overflow-hidden">
-      <h2 className="md:text-6xl text-4xl font-bold mb-12 text-center">
-        My <span className="text-transparent bg-gradient-to-r from-teal-400 to-teal-600 bg-clip-text">work experience</span>
+      <h2 className="md:text-6xl text-4xl font-bold mb-6 text-center">
+        My <span className="text-transparent bg-gradient-to-r from-teal-500 to-teal-600 bg-clip-text">work experience</span>
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-12 mx-auto">
-        {expertiseData.map((item) => (
-          <motion.div
-            key={item.title}
-            className={`flex flex-col items-center p-8 rounded-xl border shadow-lg transition-transform ${
-              item.title === "Frontend Developer" ? "border-8 border-teal-600" : "border-gray-300"
-            }`}
-            animate={item.title === "Frontend Developer" ? borderControls : {}}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-x-4 mx-auto px-4">
+        {/* Frontend Developer Card */}
+        <motion.div
+          key="Frontend Developer"
+          className={`flex flex-col items-start justify-center px-6 rounded-lg shadow-lg transition-transform border-8 border-teal-600 md:col-span-1 md:row-span-2 mb-4 md:mb-0 glow-effect`} // Add glow-effect class
+          animate={borderControls}
+        >
+          <img src={expertiseData[1].icon} alt={expertiseData[1].title} className="w-36 h-36 mb-2 object-contain md:w-48 md:h-48" />
+          <motion.span
+            className={`text-3xl md:text-6xl font-semibold mb-1 text-teal-600`}
+            animate={titleControls}
           >
-            <img src={item.icon} alt={item.title} className="w-32 h-32 mb-4 object-contain" />
-            <motion.span
-              className={`text-2xl font-semibold mb-2 ${
-                item.title === "Frontend Developer" ? "text-teal-600 text-4xl" : "text-3xl"
-              }`}
-              animate={item.title === "Frontend Developer" ? titleControls : {}}
-            >
-              {item.title}
-            </motion.span>
-            <p className="text-base text-center text-gray-700">{item.description}</p>
-          </motion.div>
-        ))}
+            {expertiseData[1].title}
+          </motion.span>
+          <p className="text-base text-gray-700 mb-4 leading-relaxed md:text-xl">{expertiseData[1].description}</p>
+        </motion.div>
+
+        {/* Other Cards stacked vertically on the right */}
+        <div className="flex flex-col justify-between h-full">
+          {/* Fullstack Developer Card */}
+          {expertiseData
+            .filter(item => item.title === "Fullstack Developer")
+            .map((item) => (
+              <motion.div
+                key={item.title}
+                className={`flex flex-col items-start px-6 rounded-lg border shadow-lg transition-transform border-gray-300 mb-4`}
+              >
+                <img src={item.icon} alt={item.title} className="w-32 h-32 mb-2 object-contain" />
+                <span className="text-2xl md:text-3xl font-semibold mb-1">{item.title}</span>
+                <p className="text-base text-gray-700 mb-4 leading-relaxed">{item.description}</p>
+              </motion.div>
+          ))}
+
+          {/* Team Lead Card */}
+          {expertiseData
+            .filter(item => item.title === "Team Lead")
+            .map((item) => (
+              <motion.div
+                key={item.title}
+                className={`flex flex-col items-start px-6 rounded-lg border shadow-lg transition-transform border-gray-300`}
+              >
+                <img src={item.icon} alt={item.title} className="w-32 h-32 mb-2 object-contain md:w-36 md:h-36" />
+                <span className="text-2xl md:text-3xl font-semibold mb-1">{item.title}</span>
+                <p className="text-base text-gray-700 mb-4 leading-relaxed">{item.description}</p>
+              </motion.div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/data/expertise.js
+++ src/data/expertise.js
@@ -3,19 +3,19 @@ import expertise2 from "../assets/expertise2.svg";
 import expertise3 from "../assets/expertise3.svg";
 
 export const expertiseData = [
-    {
-      title: "Fullstack Developer",
-      description: "Experienced in building end-to-end solutions, handling both frontend and backend development.",
-      icon: expertise2,
-    },
-    {
-      title: "Frontend Developer",
-      description: "Specialized in creating responsive and interactive user interfaces using modern frontend technologies like React and Next.js.",
-      icon: expertise1
-    },
-    {
-      title: "Team Lead",
-      description: "Proven track record of leading development teams, managing projects, and guiding technical decisions.",
-      icon: expertise3,
-    },
-  ];
+  {
+    title: "Fullstack Developer",
+    description: "Experienced in developing end-to-end solutions that encompass both frontend and backend development. Proficient in Node.js, Firebase, and PostgreSQL for backend technologies, and skilled in utilizing React, Next.js, and Tailwind CSS for crafting dynamic user interfaces.",
+    icon: expertise2,
+  },
+  {
+    title: "Frontend Developer",
+    description: "Specialized in creating responsive and interactive user interfaces using modern frontend technologies like React, Next.js, and TypeScript. Expertise in managing complex state, optimizing performance, implementing animations, and adhering to UI/UX design principles.",
+    icon: expertise1,
+  },
+  {
+    title: "Team Lead",
+    description: "Proven track record of leading teams of 3-8 developers through projects from start to finish. Experienced in offering code reviews, mentorship, and high-level problem-solving while fostering effective communication and collaboration among stakeholders.",
+    icon: expertise3,
+  },
+];
