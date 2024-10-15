986ce05bfea0912915a106dffe32dd6b372cdc3a
Reza <homayunir@gmail.com>
2024-10-15 15:38:46 -0400

    Updated ProjectTags to not be fully uppercase

--- src/Components/ProjectTag.jsx
+++ src/Components/ProjectTag.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 const ProjectTag = ({ text, bgColor, textColor }) => {
   return (
     <h1
-      className={`text-sm font-bold uppercase px-3 py-1 rounded-full ${bgColor} ${textColor} shadow-md glow-effect`}
+      className={`text-sm font-bold px-3 py-1 rounded-full ${bgColor} ${textColor} shadow-md glow-effect`}
     >
       {text}
     </h1>
